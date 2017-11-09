0104e7796c3156b1cf36397d36de4f685f630cc9
Trairat Punyawatthananukool <samoooop@gmail.com>
2017-11-09 07:29:43 +0700

    add number of thread as third argument

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+rm result/$1.txt
 for i in {1..20..1}
 	do
 		echo "running using $i thread(s)"
 		export OMP_NUM_THREADS=$i
 		icc -qopenmp -std=c++11 main.cpp
-		./a.out $1 125000 >> result/$1.txt
+		./a.out $1 $2 $i >> result/$1.txt
 done
